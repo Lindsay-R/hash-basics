af2e5d9e8a41fe6f6b7bc75256748493a54f8910
Lindsay-R <lrecknell@gmail.com>
2014-06-19 16:50:34 -0600

    05_print_address.rb... 2nd revise

--- 05_print_address.rb
+++ 05_print_address.rb
@@ -2,4 +2,5 @@ require_relative "person"
 
 # Print Bjorn's address. It should read "444 Borg Lane, San Francisco, CA, 94104"
 address = BJORN_BORG["address"].values.inspect
-puts address
+address_formated = ((address.gsub "[", "").gsub "]", "").gsub '"', ""
+puts address_formated
