b6df09fe73a47049e945ace829df47f455134579
Lindsay-R <lrecknell@gmail.com>
2014-06-19 14:14:58 -0600

    06_favorite_food.rb... Done

--- 06_favorite_food.rb
+++ 06_favorite_food.rb
@@ -1,3 +1,7 @@
 require_relative "person"
 
 # Print Bjorn's favorite foods. It should read "Bjorn's favorite foods are sushi, hamburgers, and mexican food."
+
+foods = "#{BJORN_BORG["last_name"]}'s favorite foods are #{BJORN_BORG["favorite_foods"][0]}, #{BJORN_BORG["favorite_foods"][1]}, and #{BJORN_BORG["favorite_foods"][2]}."
+
+puts foods
