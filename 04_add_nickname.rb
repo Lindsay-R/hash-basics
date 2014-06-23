bf9727e5a86f69df632b57f2b8bf12d15adf37ea
Lindsay-R <lrecknell@gmail.com>
2014-06-23 10:26:10 -0600

    done

--- 04_add_nickname.rb
+++ 04_add_nickname.rb
@@ -1,5 +1,6 @@
 require_relative "person"
 
 # Add a nickname to the BJORN_BORG hash and print it out. Don't edit the person.rb file!
-BJORN_BORG["nick_name"]= "BB"
+nickname="BB"
+BJORN_BORG["nick_name"]= nickname
 puts BJORN_BORG["nick_name"]
